09a3e91134f6bcebc1a232ec721baeb7a6c50d4d
Gee <1176243316@qq.com>
2020-06-05 19:24:37 +0800

    service模块引入Swagger

--- service/service-edu/src/main/java/com/gee/edu/EduApplication.java
+++ service/service-edu/src/main/java/com/gee/edu/EduApplication.java
@@ -2,8 +2,10 @@ package com.gee.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.gee")
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class);
