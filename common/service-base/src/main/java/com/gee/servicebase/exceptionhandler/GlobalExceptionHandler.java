cf6ea44e30ee6c73444ae5eccfa1a5dd8947baf0
Gee <1176243316@qq.com>
2020-06-06 11:57:50 +0800

    创建自定义异常类，并处理自定义异常

--- common/service-base/src/main/java/com/gee/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ common/service-base/src/main/java/com/gee/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -7,19 +7,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	//出现任何异常都会执行
-	@ExceptionHandler(Exception.class)
-	@ResponseBody
-	public R error(Exception e){
-		e.printStackTrace();
-		return R.error().message("执行了全局异常处理");
-	}
+    //出现任何异常都会执行
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public R error(Exception e) {
+        e.printStackTrace();
+        return R.error().message("执行了全局异常处理");
+    }
 
-	//特定异常处理
-	@ExceptionHandler(NullPointerException.class)
-	@ResponseBody
-	public R error(NullPointerException e){
-		e.printStackTrace();
-		return R.error().message("执行了空指针异常处理");
-	}
+    //特定异常处理
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseBody
+    public R error(NullPointerException e) {
+        e.printStackTrace();
+        return R.error().message("执行了空指针异常处理");
+    }
+
+    //特定异常处理
+    @ExceptionHandler(GuliException.class)
+    @ResponseBody
+    public R error(GuliException e) {
+        e.printStackTrace();
+        return R.error().message(e.getCode() + e.getMsg());
+    }
 }
