ee97da399dd4046c26f4e68c7e2b26b1b4a743bc
Gee <1176243316@qq.com>
2020-06-06 11:52:58 +0800

    添加特定异常处理

--- common/service-base/src/main/java/com/gee/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ common/service-base/src/main/java/com/gee/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -12,6 +12,14 @@ public class GlobalExceptionHandler {
 	@ResponseBody
 	public R error(Exception e){
 		e.printStackTrace();
-		return R.error();
+		return R.error().message("执行了全局异常处理");
+	}
+
+	//特定异常处理
+	@ExceptionHandler(NullPointerException.class)
+	@ResponseBody
+	public R error(NullPointerException e){
+		e.printStackTrace();
+		return R.error().message("执行了空指针异常处理");
 	}
 }
