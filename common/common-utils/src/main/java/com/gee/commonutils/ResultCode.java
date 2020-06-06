d6f73873832ca50430576d9c0b31d9d097926f2e
Gee <1176243316@qq.com>
2020-06-06 09:57:42 +0800

    统一结果返回类型并使用

---
+++ common/common-utils/src/main/java/com/gee/commonutils/ResultCode.java
@@ -0,0 +1,6 @@
+package com.gee.commonutils;
+
+public interface ResultCode {
+    Integer SUCCESS = 200;
+    Integer ERROR = 500;
+}
--- service/service-edu/src/main/java/com/gee/edu/controller/TeacherController.java
+++ service/service-edu/src/main/java/com/gee/edu/controller/TeacherController.java
@@ -1,8 +1,11 @@
 package com.gee.edu.controller;
 
 
+import com.gee.commonutils.R;
 import com.gee.edu.entity.Teacher;
 import com.gee.edu.service.TeacherService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -22,15 +25,24 @@ public class TeacherController {
     @Resource
     private TeacherService teacherService;
 
+    //查询所有讲师数据
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public List<Teacher> findAllTeacher(){
-        return teacherService.list(null);
+    public R findAllTeacher() {
+        List<Teacher> list = teacherService.list(null);
+        return R.ok().data("items", list);
     }
 
+    //逻辑删除
+    @ApiOperation(value = "根据ID逻辑删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@PathVariable String id){
-        return teacherService.removeById(id);
+    public R removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {
+        boolean flag = teacherService.removeById(id);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
     }
-
 }
 
