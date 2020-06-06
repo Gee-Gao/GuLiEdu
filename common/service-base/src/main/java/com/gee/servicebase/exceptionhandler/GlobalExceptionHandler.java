ea0cc6afa271b7520bd7d532d20cbc9959b039d3
Gee <1176243316@qq.com>
2020-06-06 13:18:44 +0800

    防止日志冲突，删除原yml配置文件中日志设置的内容，然后添加xml文件，然后在异常处理类中添加日志输出

--- common/service-base/src/main/java/com/gee/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ common/service-base/src/main/java/com/gee/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.gee.servicebase.exceptionhandler;
 
 import com.gee.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     //出现任何异常都会执行
     @ExceptionHandler(Exception.class)
@@ -27,6 +29,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(GuliException.class)
     @ResponseBody
     public R error(GuliException e) {
+        log.error(e.getMessage());
         e.printStackTrace();
         return R.error().message(e.getCode() + e.getMsg());
     }
