d48d3addb8f1628207d4be547be4d628a60852f1
Gee <1176243316@qq.com>
2020-06-07 11:05:28 +0800

    解决跨域

--- service/service-edu/src/main/java/com/gee/edu/controller/LoginController.java
+++ service/service-edu/src/main/java/com/gee/edu/controller/LoginController.java
@@ -2,13 +2,11 @@ package com.gee.edu.controller;
 
 import com.gee.commonutils.R;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("eduservice/user")
+@CrossOrigin//解决跨域
 public class LoginController {
     //用户登录
     @ApiOperation("用户登录")
