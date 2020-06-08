800fbeba414d968ad3d0e2ed043c6f4c48e905c0
Gee <1176243316@qq.com>
2020-06-08 10:01:11 +0800

    前端管理端实现根据讲师id跳转修改页面

--- guli-admin/src/router/index.js
+++ guli-admin/src/router/index.js
@@ -55,6 +55,13 @@ export const constantRouterMap = [
         name: '添加讲师',
         component: () => import('@/views/edu/teacher/save'),
         meta: { title: '添加讲师', icon: 'tree' }
+      },
+      {
+        path: 'edit/:id',
+        name: '修改讲师',
+        component: () => import('@/views/edu/teacher/save'),
+        meta: { title: '修改讲师',noCache:true},
+        hidden:true
       }
     ]
   },
