5f4acc8e5bf78906803e29cd3b3a67970f22e1d9
Gee <1176243316@qq.com>
2020-07-13 15:39:14 +0800

    前端管理端页面实现添加章节

--- guli-admin/src/views/edu/course/chapter.vue
+++ guli-admin/src/views/edu/course/chapter.vue
@@ -10,6 +10,24 @@
       <el-step title="最终发布"/>
     </el-steps>
 
+    <el-button type="text" style="margin-left: 12.5%" @click="dialogChapterFormVisible=true">添加章节</el-button>
+
+    <!-- 添加和修改章节表单 -->
+    <el-dialog :visible.sync="dialogChapterFormVisible" title="添加章节">
+      <el-form :model="chapter" label-width="120px">
+        <el-form-item label="章节标题">
+          <el-input v-model="chapter.title"/>
+        </el-form-item>
+        <el-form-item label="章节排序">
+          <el-input-number v-model="chapter.sort" :min="0" controls-position="right"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogChapterFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveOrUpdate">确 定</el-button>
+      </div>
+    </el-dialog>
+
     <!-- 章节 -->
     <ul class="chanpterList">
       <li
@@ -32,7 +50,7 @@
       </li>
     </ul>
     <div>
-      <el-button @click="previous">上一步</el-button>
+      <el-button style="margin-left: 12%" @click="previous">上一步</el-button>
       <el-button :disabled="saveBtnDisabled" type="primary" @click="next">下一步</el-button>
     </div>
   </div>
@@ -47,6 +65,8 @@
         saveBtnDisabled: false, // 保存按钮是否禁用
         courseId: "",//课程id
         chapterVideoList: [],
+        dialogChapterFormVisible:false,
+        chapter:{}
       }
     },
 
