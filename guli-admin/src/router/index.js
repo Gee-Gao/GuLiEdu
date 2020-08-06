cbb7e9480a3ff811eba7a1ced647b2200612b9cc
Gee <1176243316@qq.com>
2020-08-06 15:54:41 +0800

    前端管理端实现注册统计

--- guli-admin/src/router/index.js
+++ guli-admin/src/router/index.js
@@ -10,17 +10,17 @@ Vue.use(Router)
 import Layout from '../views/layout/Layout'
 
 /**
-* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
-* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
-*                                if not set alwaysShow, only more than one route under the children
-*                                it will becomes nested mode, otherwise not show the root menu
-* redirect: noredirect           if `redirect:noredirect` will no redirect in the breadcrumb
-* name:'router-name'             the name is used by <keep-alive> (must set!!!)
-* meta : {
+ * hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+ *                                if not set alwaysShow, only more than one route under the children
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noredirect           if `redirect:noredirect` will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
     title: 'title'               the name show in submenu and breadcrumb (recommend set)
     icon: 'svg-name'             the icon show in the sidebar,
   }
-**/
+ **/
 export const constantRouterMap = [
   { path: '/login', component: () => import('@/views/login/index'), hidden: true },
   { path: '/404', component: () => import('@/views/404'), hidden: true },
@@ -147,14 +147,12 @@ export const constantRouterMap = [
       },
       {
         path: 'show',
-        name: '图标显示',
+        name: '图表显示',
         component: () => import('@/views/sta/show'),
-        meta: { title: '图标显示', icon: 'tree' }
+        meta: { title: '图表显示', icon: 'tree' }
       }
     ]
   },
-
-
 
   {
     path: '/example',
