cd2b94be689efd44382d5adea24617e600d91047
Gee <1176243316@qq.com>
2021-08-20 16:44:52 +0800

    解决敏感词注入失败问题

--- common/common-utils/src/main/java/com/gee/commonutils/R.java
+++ common/common-utils/src/main/java/com/gee/commonutils/R.java
@@ -1,9 +1,11 @@
 package com.gee.commonutils;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Data
@@ -20,9 +22,10 @@ public class R {
     @ApiModelProperty(value = "返回数据")
     private Map<String, Object> data = new HashMap<String, Object>();
 
-    private R(){}
+    private R() {
+    }
 
-    public static R ok(){
+    public static R ok() {
         R r = new R();
         r.setSuccess(true);
         r.setCode(ResultCode.SUCCESS);
@@ -30,7 +33,7 @@ public class R {
         return r;
     }
 
-    public static R error(){
+    public static R error() {
         R r = new R();
         r.setSuccess(false);
         r.setCode(ResultCode.ERROR);
@@ -38,27 +41,41 @@ public class R {
         return r;
     }
 
-    public R success(Boolean success){
+    public R success(Boolean success) {
         this.setSuccess(success);
         return this;
     }
 
-    public R message(String message){
+    public R message(String message) {
         this.setMessage(message);
         return this;
     }
 
-    public R code(Integer code){
+    public R code(Integer code) {
         this.setCode(code);
         return this;
     }
 
-    public R data(String key, Object value){
+    public  R list(Page pageParam) {
+        List commentList = pageParam.getRecords();
+        //分页查询数据进行封装并返回
+        Map<String, Object> map = new HashMap<>();
+        map.put("items", commentList);
+        map.put("current", pageParam.getCurrent());
+        map.put("pages", pageParam.getPages());
+        map.put("size", pageParam.getSize());
+        map.put("total", pageParam.getTotal());
+        map.put("hasNext", pageParam.hasNext());
+        map.put("hasPrevious", pageParam.hasPrevious());
+        return this.data(map);
+    }
+
+    public R data(String key, Object value) {
         this.data.put(key, value);
         return this;
     }
 
-    public R data(Map<String, Object> map){
+    public R data(Map<String, Object> map) {
         this.setData(map);
         return this;
     }
