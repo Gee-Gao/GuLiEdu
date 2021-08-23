ac7c8bdf6a0f009dabc86ecaa984bf50e897607f
Gee <1176243316@qq.com>
2021-08-23 14:28:14 +0800

    为管理员提供评论查看和删除功能

--- service/service-edu/src/main/java/com/gee/edu/controller/CommentController.java
+++ service/service-edu/src/main/java/com/gee/edu/controller/CommentController.java
@@ -1,9 +1,17 @@
 package com.gee.edu.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.gee.commonutils.R;
+import com.gee.edu.entity.Comment;
+import com.gee.edu.service.CommentService;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -15,7 +23,30 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/edu/comment")
+@Slf4j
 public class CommentController {
+    @Resource
+    private CommentService commentService;
+
+    @GetMapping("{page}/{limit}")
+    public R pageQueryCommentCondition(
+            @ApiParam(name = "page", value = "当前页码", required = true) @PathVariable Long page,
+            @ApiParam(name = "limit", value = "每页记录数", required = true) @PathVariable Long limit,
+            @ApiParam(name = "courseQuery", value = "查询对象") String courseId) {
+        Page<Comment> pageParam = new Page<>(page, limit);
+        commentService.page(pageParam, new LambdaQueryWrapper<Comment>()
+                .eq(!StringUtils.isEmpty(courseId), Comment::getCourseId, courseId)
+                .orderByDesc(Comment::getGmtModified));
+        log.info("评论列表" + pageParam.getRecords());
+        return R.ok().list(pageParam);
+    }
+
+    @DeleteMapping
+    public R deleteCommon(@PathVariable String id) {
+        log.info("评论id" + id);
+        commentService.removeById(id);
+        return R.ok();
+    }
 
 }
 
