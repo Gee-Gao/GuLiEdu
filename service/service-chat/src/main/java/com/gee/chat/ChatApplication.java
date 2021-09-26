fe48a5d6ab69f49cfd28e5c4f0683028f5d88fc8
Gee <1176243316@qq.com>
2021-09-26 15:06:27 +0800

    解决支付宝模块和客服模块Swagger不能打开

--- service/service-chat/src/main/java/com/gee/chat/ChatApplication.java
+++ service/service-chat/src/main/java/com/gee/chat/ChatApplication.java
@@ -2,8 +2,10 @@ package com.gee.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.gee")
 public class ChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
--- service/service-zfb/src/main/java/com/gee/zfb/ZfbApplication.java
+++ service/service-zfb/src/main/java/com/gee/zfb/ZfbApplication.java
@@ -2,7 +2,9 @@ package com.gee.zfb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.gee")
 @SpringBootApplication
 public class ZfbApplication {
     public static void main(String[] args) {
